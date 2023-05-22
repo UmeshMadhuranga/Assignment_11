65be59fd501a3e1b24cd92cbd424d46897c17504
UmeshMadhuranga <122043239+UmeshMadhuranga@users.noreply.github.com>
2023-05-22 14:38:53 +0530

    init

---
+++ controller/CustomerController.js
@@ -0,0 +1,7 @@
+import {Customer} from "../model/Customer.js";
+
+document.getElementById('btnClearC').addEventListener('click', () => {
+    // let customer = new Customer(1, "namal", "siripade", "0751234567");
+
+    console.log("clicked");
+})
